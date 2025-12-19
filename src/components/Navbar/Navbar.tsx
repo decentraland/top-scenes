e50f49f297abb9a8688ff0a4dc42ddb3fc13dbb3
alejandralevy <alelevy15@gmail.com>
2025-12-19 10:38:14 -0300

    feat: fix navabar and implement page tracking hook

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,19 +3,6 @@ import { Navbar as BaseNavbar } from "decentraland-ui2"
 import { NavbarProps } from "./types"
 import { useAuth } from "../../contexts/auth"
 
-const CDN_RELEASES = {
-  Windows: {
-    amd64:
-      "https://explorer-artifacts.decentraland.org/launcher-rust/Decentraland_installer.exe",
-  },
-  macOS: {
-    amd64:
-      "https://explorer-artifacts.decentraland.org/launcher/dcl/Decentraland%20Outdated-mac-x64.dmg",
-    arm64:
-      "https://explorer-artifacts.decentraland.org/launcher-rust/Decentraland_installer.dmg",
-  },
-}
-
 export const Navbar: FC<NavbarProps> = (props) => {
   const { avatar, wallet, isSignedIn, signIn, signOut } = useAuth()
 
@@ -28,7 +15,6 @@ export const Navbar: FC<NavbarProps> = (props) => {
       isSignedIn={isSignedIn}
       onClickSignIn={signIn}
       onClickSignOut={signOut}
-      cdnLinks={CDN_RELEASES}
     />
   )
 }
--- src/components/Pages/TopScenesPage/TopScenesPage.tsx
+++ src/components/Pages/TopScenesPage/TopScenesPage.tsx
@@ -1,4 +1,6 @@
 import { type FC, memo } from "react"
+import { useLocation } from "react-router-dom"
+import { usePageTracking } from "@dcl/hooks"
 import { Banner } from "../../Banner"
 import { LiveLeaderboard } from "../../LiveLeaderboard"
 import { MobileTabs } from "../../MobileTabs"
@@ -6,6 +8,9 @@ import { PreviousWinners } from "../../PreviousWinners"
 import { ContentWrapper, PageContainer } from "./TopScenesPage.styled"
 
 export const TopScenesPage: FC = memo(() => {
+  const location = useLocation()
+  usePageTracking(location.pathname)
+
   return (
     <PageContainer>
       <Banner />
